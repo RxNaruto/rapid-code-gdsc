c2ca3214048bab50a52ecf8da2b6de653ff1286a
Rithk <rithkchaudhary86@gmail.com>
2024-02-23 21:41:59 +0530

    all files

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,26 @@
-import {Home} from "./Pages/Home"
-const App=()=>{
-  return(
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./Pages/Signup";
+import { Signin } from "./Pages/Signin";
+import { Dashboard } from "./Pages/Dashboard";
+import { SendMoney } from "./Pages/SendMoney";
+
+function App() {
+  return (
     <>
-    <Home />
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
-export default App;
+
+export default App
